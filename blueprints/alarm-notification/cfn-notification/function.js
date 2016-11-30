8478aed87a9ad85e3adae36561ffaa1920abfaf9
Julian Kleinhans <julian.kleinhans@aoe.com>
2016-11-30 21:49:50 +0100

    Update IoT panic button

--- blueprints/alarm-notification/cfn-notification/function.js
+++ blueprints/alarm-notification/cfn-notification/function.js
@@ -9,13 +9,37 @@ var Prowl = require('node-prowl');
 var prowl = new Prowl(process.env.PROWL_API_KEY);
 
 exports.handler = function(input, context) {
-    prowl.push('YO, this is awesomez!', 'My app', { priority: 2 }, function(error, remaining) {
-        if( error ) {
-            context.failed(error);
-            return;
+    input.Records.forEach(function(record) {
+        var event = JSON.parse(record.Sns.Message);
+
+        console.log('Current battery voltage:', event.batteryVoltage);
+        console.log('Received event:', event.clickType);
+
+        var message = 'Dady, I need you asap!';
+        var title = 'High';
+
+        switch(event.clickType)
+        {
+            case "SINGLE":
+                // default above
+                break;
+            case "DOUBLE":
+                message = 'Dady, I need you NOW!';
+                title = 'Emergency';
+                break;
+            case "LONG":
+                //
+                break;
         }
 
-        console.log( 'I have ' + remaining + ' calls to the api during current hour. BOOM!' );
-        context.succeed('DONE');
+        prowl.push(message, title, { priority: 2 }, function(error, remaining) {
+            if( error ) {
+                context.fail(error);
+                return;
+            }
+
+            console.log( 'I have ' + remaining + ' calls to the api during current hour. BOOM!' );
+            context.succeed('DONE');
+        });
     });
 };
